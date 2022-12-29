c5083047b69c51de4245c3c60c91beecc4d3423f
Sushmoy Roy <sushmoyr@gmail.com>
2022-12-29 13:02:31 +0600

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import Link from "next/link";
+import {ImFacebook, ImTwitter, ImYoutube} from "react-icons/im";
+import Newsletter from "./_child/newsletter";
 
 function Footer(props) {
+    const bg = {
+        backgroundImage : "url('/images/footer.png')",
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: "bottom left"
+    }
+
     return (
-        <div>footer</div>
+        <footer className="bg-gray-50" style={bg}>
+            <Newsletter></Newsletter>
+            <div className="container mx-auto flex justify-center py-12">
+                <div className="py-5">
+                    <div className="flex gap-6">
+                        <Link href={'/'}><ImFacebook color="#888888" /></Link>
+                        <Link href={'/'}><ImTwitter color="#888888" /></Link>
+                        <Link href={'/'}><ImYoutube color="#888888" /></Link>
+                    </div>
+
+                    <p className="py-5 text-gray-400">Copyright ©2022 All rights reserved | This template is made with  by Daily Tuition</p>
+                    <p className="text-gray-400 text-center">Terms & Condition</p>
+                </div>
+            </div>
+
+        </footer>
     );
 }
 
